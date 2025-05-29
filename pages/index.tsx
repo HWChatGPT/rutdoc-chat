f5268fbbd2ce5bc62046dc3db6a6762ad18697bd
HWChatGPT <john@heatwavehunting.com>
2025-05-28 20:52:21 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function RutDocChat() {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
- {!visible && (
+{!visible && (
   <div
     className="fixed bottom-4 right-4 bg-black text-white px-4 py-2 rounded-xl shadow-lg cursor-pointer z-50"
     onClick={() => setVisible(true)}
